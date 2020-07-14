6b0c9aad5375afc5dfa0ef687daeeced399d642b
wandersick <wandersick@gmail.com>
2020-07-14 21:10:43 +0800

    First release

---
+++ FolderTemplateProvisioner.bat
@@ -0,0 +1,291 @@
+:: Name: ws-folder-template-provisioner
+:: Version: 1.0
+:: Date: 20200714
+:: Description:
+::   This Windows batch script provisions (copies) new folders with exact permissions and content
+::   from a specified existing folder (template) based on the information (first name & last name)
+::   inputted by a user via its command-line interface.
+:: 
+::   It solves a problem using 'robocopy /MIR /COPYALL' in which folders copied using Windows
+::   Explorer (a.k.a. File Explorer) may not retain unique permissions and inherit permissions
+::   from parent folder.
+:: 
+::   The script has been designed with care to improve usability and avoid accidental deletion.
+::
+:: Features: Refer to README.md
+:: 
+:: Requirements:
+::   1. Windows OS with robocopy
+::   2. Administrator rights (required by robocopy /COPYALL)
+::
+:: Script Filenames: 
+::   1. FolderTemplateProvisioner.bat (main script)
+::   2. _elevate.vbs (optional, for UAC elevation if admin rights are unavailable)
+::
+:: Setting up the Scripts:
+::   1. Edit `templateName` variable at the upper area of 'FolderTemplateProvisioner.bat' script
+::      by setting it to the folder acting as the template, e.g.
+::      '01 TEMPLATE Folder - Copy for New  Starter', with required files and permissions inside
+::   2. Place both scripts ('FolderTemplateProvisioner.bat' and optionally '_elevate.vbs') inside
+::      a folder containing 'A,B,C...Z' sub-folders, sitting beside them. The 'A-Z' folders contains
+::      the template folder and provisioned folders named 'LASTNAME, Firstname' copied by the script
+::      from the template folder
+::
+:: Folder Hierarchy: Refer to README.md
+::
+:: How to Provision a New Folder:
+::   1. Double-click FolderTemplateProvisioner.bat and follow on-screen instructions
+::   2. Input last name and first name
+::   3. Review the input
+::   4. Wait for robocopy file copy (folder template provisioning)
+::   5. Verify the created folder (which pops up optionally at the end)
+::
+:: Screenshots: Refer to README.md
+
+@echo off
+setlocal enabledelayedexpansion
+
+:: Define your template folder name here (without quotes)
+set templateName=01 TEMPLATE Folder - Copy for New  Starter
+
+:: Set the working directory where script is located by %~d0%~p0 (e.g. x:\...\here)
+set WorkingDir=%~d0%~p0
+
+:: Detect if system supports "attrib"
+attrib >nul 2>&1
+if "%errorlevel%"=="9009" set noAttrib=1
+
+if not exist "%WorkingDir%\_elevate.vbs" goto :skipAdminCheck
+
+:: UAC check
+reg query HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA | find /i "0x1">nul 2>&1
+if %errorlevel% EQU 0 set UACenabled=1
+
+:: Detect if system has WSH disabled unsigned scripts
+:: if useWINSAFER = 1, the TrustPolicy below is ignored and use SRP for this option instead. So check if = 0.
+:: if TrustPolicy = 0, allow both signed and unsigned; if = 1, warn on unsigned; if = 2, disallow unsigned.
+for /f "usebackq tokens=3 skip=2" %%a in (`reg query "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v UseWINSAFER 2^>nul`) do (
+	@if "%%a" EQU "0" (
+		@for /f "usebackq tokens=3 skip=2" %%i in (`reg query "HKLM\SOFTWARE\Microsoft\Windows Script Host\Settings" /v TrustPolicy 2^>nul`) do (
+			@if "%%i" GEQ "2" (
+				set noWSH=1
+			)
+		)
+	)
+)
+
+if defined noWSH (
+	echo.
+	echo :: Error: Windows Scripting Host is disabled.
+	echo.
+	pause
+	goto :skipAdminCheck
+)
+
+:: Detect admin rights
+if defined noAttrib goto :skipAdminCheck
+attrib -h "%windir%\system32" | find /i "system32" >nul 2>&1
+if %errorlevel% EQU 0 (
+	REM only when UAC is enabled can this script be elevated. Otherwise, non-stop prompting will occur.
+	if "%UACenabled%" EQU "1" (
+		cscript //NoLogo "%WorkingDir%\_elevate.vbs" "%WorkingDir%" "%WorkingDir%\FolderTemplateProvisioner.bat" >nul 2>&1
+		goto :EOF
+	) else (
+		echo.
+		echo :: Error: Folder Template Provisioner requires admin rights. Please run as admin.
+		echo.
+		pause
+		goto :EOF
+	)
+)
+:skipAdminCheck
+
+:: Detect admin rights (subsequent)
+
+if defined noAttrib goto :skipAdminCheckSubsequent
+attrib -h "%windir%\system32" | find /i "system32" >nul 2>&1
+if %errorlevel% EQU 0 (
+	echo.
+	echo :: Error: Folder Template Provisioner requires admin rights. Please run as admin.
+	echo.
+	pause
+	goto :EOF
+)
+
+:skipAdminCheckSubsequent
+
+title Welcome to Folder Template Provisioner
+
+:: Enter name
+:enterName
+set lastName=
+set firstName=
+set rerun=
+cls
+
+echo.
+set /p lastName=:: Please input last name ^(in capital case, e.g. DOWNEY^): 
+echo.
+set /p firstName=:: Please input first name ^(in camel case, e.g. Robert^): 
+
+:: Review folder name
+:reviewFolderName
+set folderName=
+set goAhead=
+echo.
+echo ___________________________________________________________________
+echo.
+echo :: Based on the entry, below folder will be created:
+echo.
+echo    Folder name: "%lastName%, %firstName%"
+echo.
+set /p goAhead= :: Would you like to continue? [Y,N] 
+if /i "%goAhead%" EQU "N" (
+  goto :enterName
+) else if /i "%goAhead%" EQU "Y" (
+  set folderName=%lastName%, %firstName%
+  set folderOpened=
+  goto :folderExistenceCheck
+) else (
+  goto :reviewFolderName
+)
+
+:: Folder existence check
+:folderExistenceCheck
+
+if not defined firstName (
+  echo.
+  echo ___________________________________________________________________
+  echo.
+  echo :: Error: Name is invalid. Please try again
+  echo.
+  pause
+  goto :enterName
+)
+
+if not defined lastName (
+  echo.
+  echo ___________________________________________________________________
+  echo.
+  echo :: Error: Name is invalid. Please try again
+  echo.
+  pause
+  goto :enterName
+)
+
+REM Warn user that data could be deleted, due to robocopy /MIR, if destination contains the same folder name
+set goAhead=
+set lastName1st=
+
+REM Grab first letter from last name
+set lastName1st=%lastName:~0,1%
+
+REM Enter the responsible single-letter folder
+pushd %lastName1st%>nul 2>&1
+if %errorlevel% NEQ 0 (
+  echo.
+  echo ___________________________________________________________________
+  echo.
+  echo :: Error: Target A-Z folder "%lastName1st%" is invalid or inaccessible. Please check and try again
+  echo.
+  popd
+  pause
+  goto :enterName
+)
+
+if not exist "%templateName%" (
+  echo.
+  echo ___________________________________________________________________
+  echo.
+  echo :: Error: Template folder name "%templateName%" does not exist in target location
+  echo.
+  echo    Or it is defined wrongly in the script
+  echo.
+  popd
+  endlocal
+  echo Press any key to quit . . .
+  pause >nul
+  goto :EOF
+)
+
+if exist "%folderName%" (
+  echo.
+  echo ___________________________________________________________________
+  echo.
+  echo :: Warning: A folder with the same name as "%folderName%" already exists
+  if "!folderOpened!" NEQ "1" (
+    echo.
+    echo    Please confirm it is unneeded . . . Opening the folder . . . 
+    ping 127.0.0.1 -n 2 >nul 2>&1
+    explorer "%folderName%"
+    set folderOpened=1
+  )
+  echo.
+  set /p goAhead= :: Are you sure to DELETE it and replace it with a new one? ^(Answer 'N' to quit if unsure^) [Y,N] 
+  popd
+  if /i "!goAhead!" EQU "N" (
+    goto :enterName
+  ) else if /i "!goAhead!" EQU "Y" (
+    goto :createFolder
+  ) else (
+    goto :folderExistenceCheck
+  )
+)
+
+:: Create folder
+:createFolder
+
+REM Enter the responsible single-letter folder
+pushd %lastName1st%>nul 2>&1
+echo.
+echo ___________________________________________________________________
+echo.
+echo :: Starting file copy. Please wait . . .
+echo.
+ping 127.0.0.1 -n 2 >nul 2>&1
+
+REM Copy template folder as a new folder
+set robocopyError=
+robocopy "%templateName%" "%folderName%" /MIR /COPYALL /TEE /LOG+:%Temp%\FolderCreator.log
+if %errorlevel% GTR 3 set robocopyError=%errorlevel%
+
+:: Folder created
+:folderCreated
+echo.
+echo ___________________________________________________________________
+set goAhead=
+echo.
+echo :: Completed
+echo.
+if defined robocopyError goto :robocopyError
+set /p goAhead= :: Open the new folder now? [Y,N] 
+if /i "%goAhead%" EQU "N" (
+  goto :end
+) else if /i "%goAhead%" EQU "Y" (
+  explorer "%folderName%"
+  goto :end
+) else (
+  goto :folderCreated
+)
+
+:: End
+:end
+popd
+endlocal
+cls
+echo.
+echo    Thank you for using :^)
+echo.
+echo    Have a good day!
+echo.
+ping 127.0.0.1 -n 2 >nul 2>&1
+cls
+exit
+
+:: Output robocopy error code if there as an error
+:robocopyError
+echo :: Error: Folder provisioning failed. The robocopy error code was %robocopyError%
+echo.
+echo Press any key to quit . . .
+pause >nul
+goto :EOF
---
+++ README.md
@@ -0,0 +1,139 @@
+# Folder Template Provisioner
+
+This Windows batch script provisions (copies) new folders with exact permissions and content from a specified existing folder (template) based on the information (first name and last name) inputted by a user via its command-line interface.
+
+It solves a problem using `robocopy /MIR /COPYALL` in which folders copied using Windows Explorer (a.k.a. File Explorer) may not retain unique permissions and inherit permissions from parent folder.
+
+The script has been designed with care to improve usability and avoid accidental deletion.
+
+## Features
+
+- Accept input from user
+  - First name and last name
+- Copy a new folder using specified template folder retaining exact permissions and content using `robocopy /MIR /COPYALL` named in `LASTNAME, First Name` format according to user input
+- Protection
+  - Guard against data loss if target folder already exists
+    - Target folder is popped up for user to check
+    - Confirmation from user is required before continuing
+  - Validate source template folder and target folder before running `robocopy`
+- Automatic UAC prompt
+  - Automatically elevate the script to run it as admin if UAC (User Account Control) is enabled but admin rights are unavailable. This requires an optional script in the same folder (`_elevate.vbs`)
+- Error checking and messages available
+  - Windows Script Host is unavailable (for the UAC elevation VBScript)
+  - Admin rights are unavailable
+  - Invalid first name / last name
+  - Target `A-Z` folder is invalid or inaccessible
+  - Template folder name does not exist in target location or wrongly defined in the script
+  - Target folder already exists, requiring user to confirm
+  - File copy (folder template provisioning) failure and the `robocopy` return code
+- Confirmation prompt
+  - Names inputted by user are shown again for review and confirmation
+  - A folder with the same name already exists, where confirmation is required from user before continuing, as `robocopy /MIR` (mirror), the command responsible for copying folder templates, would result in loss of data in existing folder
+- Pop up target folder automatically for convenience
+  - Optionally at the end of successful folder creation
+  - Or, when a folder with the same name already exists, where confirmation is required from user before continuing
+- Logging
+  - A `robocopy` log file is saved and appended to `%Temp%\FolderCreator.log`. As this is not meant to be permanent, it can get deleted by system shortly afterwards
+
+## Requirements
+
+1. Windows OS with `robocopy`
+2. Administrator rights (required by `robocopy /COPYALL`, referring to the last screenshot in this README document)
+
+## Script Filenames
+
+1. `FolderTemplateProvisioner.bat` (main script)
+2. `_elevate.vbs` (optional, for UAC elevation if admin rights are unavailable, provided that User Account Control is enabled in the system)
+
+## Folder Hierarchy
+
+```c
+│ FolderTemplateProvisioner.bat // main script
+│ _elevate.vbs // UAC elevation script (optional)
+│
+├───A
+│   ├───Template Folder // By default: '01 TEMPLATE Folder - Copy for New  Starter'
+│   │                   // containing folders/files of specific permissions to copy
+│   │───LASTNAME, Firstname 1
+│   │───LASTNAME, Firstname 2
+│   │                       .  // Each copied folder is named in this way according
+│   │                       .  // to names inputted by user with sub-folders and files
+│   │                       .  // retaining specific permissions
+│   └───LASTNAME, Firstname n
+│
+├───B
+├───C
+│   .
+│   .   // Created folders are placed in folders according to initials from user input
+│   .
+└───Z
+```
+
+## Steps by Step - How to Set up the Scripts
+
+1. Edit `templateName` variable at the upper area of `FolderTemplateProvisioner.bat` script by setting it to the folder acting as the template, e.g. `01 TEMPLATE Folder - Copy for New  Starter`, with required files and permissions inside
+2. Place both scripts (`FolderTemplateProvisioner.bat` and optionally `_elevate.vbs`) inside a folder containing `A,B,C...Z` sub-folders, sitting beside them. The `A-Z` folders contains the template folder and provisioned folders named '`LASTNAME, Firstname`' copied by the script from the template folder
+
+## Step by Step - How to Provision a New Folder
+
+1. Double-click `FolderTemplateProvisioner.bat` and follow on-screen instructions
+2. Input last name and first name
+3. Review the input
+4. Wait for `robocopy` file copy (folder template provisioning)
+5. Verify the created folder (which pops up optionally at the end)
+
+## Screenshots - Provisioning a New Folder with the Script
+
+A. Run `FolderTemplateProvisioner.bat`
+
+![Run FolderTemplateProvisioner.bat](_images/00-run-folder-template-provisioner.png)
+
+B. UAC elevation is prompted automatically when admin rights are unavailable, provided that an optional `_elevate.vbs` exists in the same folder
+
+![UAC elevation is prompted automatically when admin rights are unavailable, provided that an optional _elevate.vbs exists in the same folder](_images/01-automatic-uac-prompt.png)
+
+C. Input first name and last name
+
+![Input first name and last name](_images/02-input-firstname-lastname.png)
+
+D. Review and confirm input
+
+![Review and confirm input](_images/03-review-and-confirm-input.png)
+
+E. Robocopy provisions new folder from folder template with exact content and permissions
+
+![Robocopy provisions new folder from folder template with exact content and permissions](_images/04-robocopy-provisions-new-folder-from-folder-template.png)
+
+F. Open folder after completion (optional)
+
+![Open folder after completion](_images/05-open-folder-after-completion.png)
+
+G. Verify created folder via the opened Windows Explorer
+
+![Verify created folder](_images/06-new-folder-created.png)
+
+## Screenshots - Error Messages
+
+A. Target folder already exists
+
+![Error - Target folder already exists](_images/e01-target-folder-already-exists.png)
+
+B. Name inputted by user is invalid
+
+![Error - Name inputted by user is invalid](_images/e02-name-inputted-by-user-is-invalid.png)
+
+C. Target `A-Z` folder is invalid or inaccessible
+
+![Error - Target A-Z folder is invalid or inaccessible](_images/e03-target-az-folder-invalid-or-inaccessible.png)
+
+D. Source template folder does not exist or is wrongly defined within script
+
+![Error - Source template folder does not exist or is wrongly defined within script](_images/e04-source-template-folder-does-not-exist.png)
+
+E. No admin rights are detected, while UAC is off and/or the optional `_elevate.vbs` is not in the same folder
+
+![Error - No admin rights are detected, while UAC is off and/or the optional _elevate.vbs is not in the same folder](_images/e05-no-admin-rights-and-cannot-elevate-by-uac.png)
+
+F. Robocopy error ends with a return code presented to user
+
+![Error - Robocopy error ends with a return code presented to user](_images/e06-robocopy-error-with-return-code.png)
